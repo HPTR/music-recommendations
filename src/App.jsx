6c463392055a0585a151ab26ffe77301ad29e2e7
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-13 21:42:07 +0100

    Add test recommendation modal element

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.scss';
+import RecommendationsGallery from './containers/RecommendationsGallery/RecommendationsGallery';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./assets/sass/reset.scss"
 
 function App() {
   return (
     <div className="App">
       <Router>
       <Routes>
-        <Route
-          path="/"
-          element={<Marketplace />}
-        />
+        <Route path="/" element={<RecommendationsGallery />} />
       </Routes>
       </Router>
     </div>
--- src/containers/RecommendationsGallery/RecommendationsGallery.jsx
+++ src/containers/RecommendationsGallery/RecommendationsGallery.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import RecommendationModal from '../../components/RecommendationModal/RecommendationModal';
+import { useState } from 'react';
 
 const RecommendationsGallery = () => {
+
+    const [show, setShow] = useState(false);
+
+    const toggleModal = () => {
+      setShow(!show);
+    }
+
+
   return (
     <div>
-        
+        <h1>HELLO</h1>
+        <button onClick={toggleModal} >Modal</button>
+        <RecommendationModal show={show} onClose={toggleModal} />
     </div>
   )
 }
