987c1636a8a829fd0d4c6f5a91cb296bb28b6a6f
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-13 20:33:38 +0100

    Add initial routing to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-
+      <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Marketplace />}
+        />
+      </Routes>
+      </Router>
     </div>
   );
 }
