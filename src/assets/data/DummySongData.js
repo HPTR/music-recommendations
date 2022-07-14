490d461023b77192ccf5eb71603f4dc2968ac956
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-14 22:25:44 +0100

    Add dummy song data for test component rendering

---
+++ src/assets/data/DummySongData.js
@@ -0,0 +1,32 @@
+const songArray = [
+    {
+        id: 0,
+        songName: "If You Wanna",
+        artistName: "The Vaccines",
+        albumName: "What Did You Expect From The Vaccines?",
+        image: "https://via.placeholder.com/300x301"
+    },
+    {
+        id: 1,
+        songName: "Overachiever",
+        artistName: "Walking On Rivers",
+        albumName: "Overachiever",
+        image: "https://via.placeholder.com/300x301"
+    },
+    {
+        id: 2,
+        songName: "Cleopatra",
+        artistName: "Mel Blue",
+        albumName: "Cleopatra",
+        image: "https://via.placeholder.com/300x301"
+    },
+    {
+        id: 3,
+        songName: "why you so sad?",
+        artistName: "Wax Works",
+        albumName: "why you so sad? EP",
+        image: "https://via.placeholder.com/300x301"
+    }
+];
+
+export default songArray;
