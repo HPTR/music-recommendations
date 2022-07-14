9633e47c7a62825672f5a0291f7831ecfcf02be7
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-14 22:27:14 +0100

    Add recommendation component elements

--- src/components/Recommendation/Recommendation.jsx
+++ src/components/Recommendation/Recommendation.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import "./Recommendation.scss";
 
-const Recommendation = () => {
+const Recommendation = ({title, artist, album, image}) => {
   return (
-    <div>
-        
+    <div className="recommendation">
+        <img src={image} className="recommendation__image" alt={album}/>
+        <div className="recommendation__info">
+          <h2 className="recommendation__title">{title}</h2>
+          <h3 className="recommendation__artist">{artist}</h3>
+          <h4 className="recommendation__album">{album}</h4>
+        </div>
     </div>
   )
 }
