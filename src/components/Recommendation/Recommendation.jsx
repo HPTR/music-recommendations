e75ef65e9eff1327afb877f08f264bb121431c92
HPTR <76168685+HPTR@users.noreply.github.com>
2022-07-19 01:03:27 +0100

    Update recommendation styling

--- src/components/Recommendation/Recommendation.jsx
+++ src/components/Recommendation/Recommendation.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./Recommendation.scss";
 
-const Recommendation = ({title, artist, album, image}) => {
+const Recommendation = ({title, artist, album, image, id, onClick}) => {
   return (
-    <div className="recommendation">
-        <img src={image} className="recommendation__image" alt={album}/>
+    <div className="recommendation" >
+        <img src={image} className="recommendation__image" alt={album} onClick={onClick} id={id}/>
         <div className="recommendation__info">
           <h2 className="recommendation__title">{title}</h2>
           <h3 className="recommendation__artist">{artist}</h3>
